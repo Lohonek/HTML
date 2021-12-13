c12b999ecae91b939ddcf2152dd1e324905a070f
nyku <dawid.rec5@gmail.com>
2021-12-13 17:42:56 +0100

    Add index.html

--- JS/Warunki/app.js
+++
@@ -1,8 +0,0 @@
-var kolor = prompt("Podaj kolor: 1-zielony lub 2-czerwony");
-if ((kolor == 1) | (kolor == "zielony")) {
-  document.write("<font color=green>zielony</font>");
-} else if ((kolor == 2) | (kolor == "czerwony")) {
-  document.write("<font color=red>czerwony</font>");
-} else {
-  document.write("Podałeś nieprawidłową wartość !");
-}
