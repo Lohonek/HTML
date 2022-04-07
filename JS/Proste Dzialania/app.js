418c90bf19e3db20880cb1607ee7bb083ac6ec21
nyku <dawid.rec5@gmail.com>
2022-04-07 22:33:28 +0200

    Fix

--- JS/Proste Dzialania/app.js
+++ JS/Proste Dzialania/app.js
@@ -1,23 +1,60 @@
 function dodawanie() {
   var n1 = parseFloat(document.getElementById("n1").value);
   var n2 = parseFloat(document.getElementById("n2").value);
-  document.getElementById("n3").value = n1 + n2;
+
+  if (
+    document.getElementById("n1").value.length == 0 ||
+    document.getElementById("n2").value.length == 0
+  ) {
+    document.getElementById("wynik").innerHTML = 'Prosze uzupelnic dwie liczby'
+  } else {
+    var n3 = (n1 + n2)
+    document.getElementById("wynik").innerHTML = 'Wynik dzialania wynosi: ' + n3;
+  }
 }
 
 function odejmowanie() {
   var n1 = parseFloat(document.getElementById("n1").value);
   var n2 = parseFloat(document.getElementById("n2").value);
-  document.getElementById("n3").value = n1 - n2;
+  if (
+    document.getElementById("n1").value.length == 0 ||
+    document.getElementById("n2").value.length == 0
+  ) {
+    document.getElementById("wynik").innerHTML = 'Prosze uzupelnic dwie liczby'
+  } else {
+    var n3 = (n1 - n2)
+    document.getElementById("wynik").innerHTML = 'Wynik dzialania wynosi: ' + n3;
+  }
 }
 
 function mnozenie() {
   var n1 = parseFloat(document.getElementById("n1").value);
   var n2 = parseFloat(document.getElementById("n2").value);
-  document.getElementById("n3").value = n1 * n2;
+  if (
+    document.getElementById("n1").value.length == 0 ||
+    document.getElementById("n2").value.length == 0
+  ) {
+    document.getElementById("wynik").innerHTML = 'Prosze uzupelnic dwie liczby'
+  } else {
+    var n3 = (n1 * n2)
+    document.getElementById("wynik").innerHTML = 'Wynik dzialania wynosi: ' + n3;
+  }
 }
 
 function dzielenie() {
   var n1 = parseFloat(document.getElementById("n1").value);
   var n2 = parseFloat(document.getElementById("n2").value);
-  document.getElementById("n3").value = n1 / n2;
+  if (
+    document.getElementById("n1").value.length == 0 ||
+    document.getElementById("n2").value.length == 0
+  ) {
+    document.getElementById("wynik").innerHTML = 'Prosze uzupelnic dwie liczby'
+  }
+  else if(document.getElementById("n2").value == 0){ 
+    document.getElementById("wynik").innerHTML = 'Nie wolno dzielic przez zero'
+  } 
+  else {
+    var n3 = (n1 / n2)
+    document.getElementById("wynik").innerHTML = 'Wynik dzialania wynosi: ' + n3;
+  }
 }
