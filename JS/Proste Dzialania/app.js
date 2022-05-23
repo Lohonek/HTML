26bbc1eee5cc4f2f9a4cee2f33f94a951b581578
Lohonekk <dawid.rec5@gmail.com>
2022-05-23 12:08:21 +0200

    Add files

--- JS/Proste Dzialania/app.js
+++ JS/Proste Dzialania/app.js
@@ -1,4 +1,4 @@
-function obwod() {
+function pole() {
   var n1 = parseFloat(document.getElementById("n1").value);
   var n2 = parseFloat(document.getElementById("n2").value);
 
@@ -12,4 +12,20 @@ function obwod() {
     document.getElementById("wynik").innerHTML = 'Pole wynosi: ' + n3;
   }
 }
+
+function obwod() {
+  var n1 = parseFloat(document.getElementById("n1").value);
+  var n2 = parseFloat(document.getElementById("n2").value);
+
+  if (
+    document.getElementById("n1").value.length == 0 ||
+    document.getElementById("n2").value.length == 0
+  ) {
+    document.getElementById("wynik").innerHTML = 'Prosze uzupelnic dwie liczby'
+  } else {
+    var n3 = (n1 + n1 + n2 + n2)
+    document.getElementById("wynik").innerHTML = 'Obwód wynosi: ' + n3;
+  }
+}
+
 
